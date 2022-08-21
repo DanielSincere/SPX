561a18a04be40adcb93e7a87104d3a282e3165bf
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2022-08-21 19:24:23 -0400

    5.5 error on github

--- Sources/SwishLib/Runner/Runner.swift
+++ Sources/SwishLib/Runner/Runner.swift
@@ -4,18 +4,10 @@ import Sh
 final class Runner: Running {
   
   func parseSwiftPackage(cmd: String) throws -> SwiftPackageDescription {
-#if os(macOS)
-    return try sh(SwiftPackageDescription.self, "xcrun --sdk macosx \(cmd)")
-#else
     return try sh(SwiftPackageDescription.self, cmd)
-#endif
   }
   
   func exec(cmd: String) throws {
-#if os(macOS)
-    return try sh(.terminal, "xcrun --sdk macosx \(cmd)")
-#else
     return try sh(.terminal, cmd)
-#endif
   }
 }

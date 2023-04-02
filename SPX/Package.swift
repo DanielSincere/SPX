d246a4067a434b7f72bd36de6bd3df2a60d0f0f0
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2023-04-02 13:56:55 -0400

    system package

--- SPX/Package.swift
+++ SPX/Package.swift
@@ -9,10 +9,14 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.2.0"),
+    .package(url: "https://github.com/apple/swift-system", from: "1.0.0"),
   ],
   targets: [
     .executableTarget(
       name: "generate-scaffold-code",
-      dependencies: ["Sh"]),
+      dependencies: [
+        "Sh",
+        .product(name: "SystemPackage", package: "swift-system"),
+      ]),
   ]
 )
--- SPX/Sources/generate-scaffold-code/GenerateMain.swift
+++ SPX/Sources/generate-scaffold-code/GenerateMain.swift
@@ -1,6 +1,6 @@
 import Foundation
 import Sh
-import System
+import SystemPackage
 
 @main
 struct GenerateMain {

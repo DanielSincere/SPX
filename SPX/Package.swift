1c892bb8db3b83c73973646b8f7f4768cd409f3b
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-07-14 19:53:31 -0400

    upgrade Sh to 1.3.0; require Swift 5.8 and macOS 13

--- SPX/Package.swift
+++ SPX/Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
   name: "SPXScriptsForSPX",
   platforms: [
-    .macOS(.v12),
+    .macOS(.v13),
   ],
   dependencies: [
-    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.2.0"),
+    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.3.0"),
     .package(url: "https://github.com/apple/swift-system", from: "1.0.0"),
   ],
   targets: [

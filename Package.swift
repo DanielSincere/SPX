c8f08866fd62538ee9b223cd7762eb46508504d7
Full Queue Developer <103295874+FullQueueDeveloper@users.noreply.github.com>
2023-08-19 13:10:30 -0400

    upgrade Sh to 1.3.0; require Swift 5.8 and macOS 13 (#8)
    * upgrade Sh to 1.3.0; require Swift 5.8 and macOS 13
    
    * bump version
    
    ---------
    
    Co-authored-by: Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
   name: "SPX",
   platforms: [
-    .macOS(.v12),
+    .macOS(.v13),
   ],
   products: [
     .executable(name: "spx", targets: ["spx"]),
   ],
   dependencies: [
     .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.0"),
-    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.2.0"),
+    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.3.0"),
   ],
   targets: [
     .executableTarget(

eab73d76dabfc650c98a112a78d367c366231f83
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2023-08-19 16:56:50 -0400

    demo update

--- demos/VaporDemo/Package.swift
+++ demos/VaporDemo/Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
   name: "VaporDemo",
-  platforms: [.macOS(.v12)],
+  platforms: [.macOS(.v13)],
 
   dependencies: [
     .package(url: "https://github.com/vapor/vapor.git", from: "4.55.0"),

2b12236f7fa20f39d53b339f522f30bba53de399
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2024-04-18 23:00:39 -0400

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.0"),
-    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.3.0"),
+    .package(url: "https://github.com/DanielSincere/Sh.git", from: "1.3.0"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-system", from: "1.0.0"),
   ],
